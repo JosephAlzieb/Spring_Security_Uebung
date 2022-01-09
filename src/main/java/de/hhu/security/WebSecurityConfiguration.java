ff53a72a0ab0125a08a99201c78387868e153fbf
Joseph Alzieb <Joseph.Alzieb@hhu.de>
2022-01-10 00:34:37 +0100

    auf jeden Endpunkt muss man sich via github anmelden.

--- src/main/java/de/hhu/security/WebSecurityConfiguration.java
+++ src/main/java/de/hhu/security/WebSecurityConfiguration.java
@@ -15,7 +15,8 @@ public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests(a -> a
                         .antMatchers("/").permitAll()
-                        .antMatchers("/home").authenticated())
+                        .antMatchers("/home").authenticated()
+                        .antMatchers().authenticated())
                 .exceptionHandling(e -> e.authenticationEntryPoint(new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED)))
                 .csrf(c -> c.csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse()))
                 .logout(l -> l.logoutSuccessUrl("/").permitAll()).oauth2Login();
