814fed2ff86142c89e2bf263ad7d68067dff14b1
Joseph Alzieb <Joseph.Alzieb@hhu.de>
2022-01-09 21:56:28 +0100

    initial commit

---
+++ src/test/java/de/hhu/security/SecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package de.hhu.security;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
