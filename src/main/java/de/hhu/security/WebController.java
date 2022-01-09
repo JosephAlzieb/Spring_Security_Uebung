a1f9049bbc6296e84dfd967a0df0af8fe985f19a
Joseph Alzieb <Joseph.Alzieb@hhu.de>
2022-01-10 00:27:56 +0100

    auf index seite ("/") soll jeder zugreifen können. aber auf alles andere wie ("home") muss man sich via github anmelden.

--- src/main/java/de/hhu/security/WebController.java
+++ src/main/java/de/hhu/security/WebController.java
@@ -1,13 +1,14 @@
 package de.hhu.security;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class WebController {
 
     @GetMapping("/")
-    public String getIndex(){
+    public String getIndex(Model model){
         return "index";
     }
 
--- src/main/java/de/hhu/security/WebSecurityConfiguration.java
+++ src/main/java/de/hhu/security/WebSecurityConfiguration.java
@@ -14,7 +14,8 @@ public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests(a -> a
-                        .antMatchers("/").authenticated())
+                        .antMatchers("/").permitAll()
+                        .antMatchers("/home").authenticated())
                 .exceptionHandling(e -> e.authenticationEntryPoint(new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED)))
                 .csrf(c -> c.csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse()))
                 .logout(l -> l.logoutSuccessUrl("/").permitAll()).oauth2Login();
