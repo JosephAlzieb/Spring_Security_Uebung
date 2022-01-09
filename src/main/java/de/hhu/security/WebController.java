583f73f855e7bb202f86afc1514d1f9afc8f62cb
Joseph Alzieb <Joseph.Alzieb@hhu.de>
2022-01-10 00:35:18 +0100

    das zeigt alles, was auf github über den User bekannt ist.

--- src/main/java/de/hhu/security/WebController.java
+++ src/main/java/de/hhu/security/WebController.java
@@ -1,8 +1,14 @@
 package de.hhu.security;
 
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.Map;
 
 @Controller
 public class WebController {
@@ -15,5 +21,11 @@ public class WebController {
     @GetMapping("/home")
     public String getHomePage(){
         return "home";
+    }
+
+    @RequestMapping("/user")
+    @ResponseBody
+    public Map<String, Object> user(@AuthenticationPrincipal OAuth2User principal) {
+        return principal.getAttributes();
     }
 }
